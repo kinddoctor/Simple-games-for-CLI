f9de1cfcda5656f155f44e95b9b9f090d24e41c2
kinddoctor <tata-yagodina@yandex.ru>
2023-01-26 23:21:21 +0500

    add the greeting

---
+++ src/cli.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+
+export const greeting = () => { 
+  const userName = readlineSync.question('May I have your name? ');
+  return console.log('Hello, ' + userName + '!');
+}; 
