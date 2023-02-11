9f7c154c4e731a9074fe226ddc2e9fadc3421a71
kinddoctor <tata-yagodina@yandex.ru>
2023-02-11 22:42:32 +0500

    final correct game even\not even

--- src/cli.js
+++ src/cli.js
@@ -3,8 +3,7 @@ import readlineSync from 'readline-sync';
 export const greeting = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  return '$<lastInput>';
+  return console.log(`Hello, ${userName}!`);
 };
 
 export default greeting;
--- src/even-Game.js
+++ src/even-Game.js
@@ -1,10 +1,11 @@
 import readlineSync from 'readline-sync';
-import greeting from './cli.js';
 
 const isEven = number => (number % 2 === 0 ? 'yes' : 'no');
 
 export const gameIsEven = () => {
-  const userName = greeting();
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   for (let i = 1; i < 4; i += 1) {
     const number = Math.round(Math.random() * 100);
