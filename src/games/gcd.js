18678eb33efe6f7bc5d8b70f1020b87003af8ca0
kinddoctor <tata-yagodina@yandex.ru>
2023-02-21 19:27:01 +0500

    add get gsd game

---
+++ src/games/gcd.js
@@ -0,0 +1,29 @@
+import processOfGame from '../index.js';
+import { randomNumber, gcd } from '../utils.js';
+import readlineSync from 'readline-sync';
+
+const check = (number1, number2) => {
+  const nums = {};
+  if (number1 > number2) {
+   nums.big = number1;
+   nums.small = number2;
+  } else {
+    nums.big = number2;
+    nums.small = number1;
+  }
+  return gcd(nums.big, nums.small);
+};
+
+const introduction = 'Find the greatest common divisor of given numbers.';
+const gameRound = () => {
+  const number1 = randomNumber();
+  const number2 = randomNumber();
+  const userAnswer = readlineSync.question(`Question: ${number1} ${number2}\nYou answer: `);
+  const expectedAnswer = check(number1, number2).toString();
+  if (userAnswer === expectedAnswer) {
+    return 'Correct!';
+  }
+  return `'${userAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'.`;
+};
+
+export const gcdGame = processOfGame(introduction, gameRound, check);
--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,17 @@
 
 
 export const randomNumber = () => Math.round(Math.random() * 100);
+
 export const getOperator = (num) => {
   if (num % 3 === 0 || num % 7 === 0) {
     return '+';
   }
   return num % 2 === 0 ? '-' : '*';
+};
+
+export const gcd = (a, b) => {
+  if (!b) {
+    return a;
+  }
+  return gcd(b, a % b);
 };
