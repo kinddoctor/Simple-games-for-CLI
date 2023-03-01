c82415cc26c8079a8a81b12f86fcc3ec1d4874e2
kinddoctor <tata-yagodina@yandex.ru>
2023-03-01 23:33:25 +0500

    return "brain-games" game

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export const greeting = () => {
+const greeting = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   return console.log(`Hello, ${userName}!`);
