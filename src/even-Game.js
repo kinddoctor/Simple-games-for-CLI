45279348302a5678a3bd2867b09b5d42ea213cf8
kinddoctor <tata-yagodina@yandex.ru>
2023-03-02 00:08:59 +0500

    fix even-Game.js linter stuff

--- src/even-Game.js
+++ src/even-Game.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
-const isEven = number => (number % 2 === 0 ? 'yes' : 'no');
+const isEven = (number) => (number % 2 === 0 ? 'yes' : 'no');
 
-export const gameIsEven = () => {
+const gameIsEven = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
@@ -11,7 +11,7 @@ export const gameIsEven = () => {
     const number = Math.round(Math.random() * 100);
     const userAnswer = readlineSync.question(`Question: ${number}\nYou answer:`);
     const expectedAnswer = isEven(number);
-    if (expectedAnswer === userAnswer ) {
+    if (expectedAnswer === userAnswer) {
       console.log('Correct!');
     } else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'.\nLet's try again, ${userName}!`);
@@ -20,3 +20,5 @@ export const gameIsEven = () => {
   }
   console.log(`Congratulations, ${userName}!`);
 };
+
+export default gameIsEven;
