515de4e4f8510d4ab5e08d1c402a788e5b4721b9
kinddoctor <tata-yagodina@yandex.ru>
2023-03-12 14:48:30 +0500

    fix brain-prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,7 +4,7 @@ import getRandomNumber from '../utils.js';
 const middleSizeOfNumber = 100;
 
 const isPrime = (num) => {
-  for (let i = 2; i <= num / 3; i += 1) {
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) {
       return false;
     }
